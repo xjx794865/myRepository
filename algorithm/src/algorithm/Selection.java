5ad2e96a504419bfbaa92ede4883b9f0ed822253
xjx <794865413@qq.com>
2018-08-24 15:01:15 +0800

    second commit

---
+++ algorithm/src/algorithm/Selection.java
@@ -0,0 +1,52 @@
+package algorithm;
+
+public class Selection {
+
+	private Selection() {
+
+	}
+
+	public static void selectionSort(int arr[]) {
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			// 创建一个地址 存放最小索引
+			int minIndex = i;
+
+			// [i,n)的最小值
+			for (int j = i + 1; j < n; j++) {
+
+				if (arr[j] < arr[minIndex]) {
+
+					minIndex = j;
+
+					// 如果直接写 arr[i] = arr[minIndex] 只是交换地址 不交换值
+					swap(arr, i, minIndex);
+
+				}
+			}
+		}
+
+	}
+
+	public static void swap(int arr[], int i, int j) {
+
+		int tep = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tep;
+
+	}
+
+	public static void main(String[] args) {
+
+		int a[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+		Selection.selectionSort(a);
+		
+		for(int i = 0; i<a.length ;i++) {
+			System.out.print(a[i]+" ");
+		}
+
+	}
+
+}
---
+++ algorithm/src/algorithm/SelectionSort.java
@@ -0,0 +1,76 @@
+package algorithm;
+
+import com.itcast.entity.Student;
+
+public class SelectionSort {
+
+	private SelectionSort() {
+	}
+
+	public static void sort(Comparable[] arr) {
+
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+
+			int minIndex = i;
+
+			for (int j = i + 1; j < n; j++) {
+				if (arr[j].compareTo(arr[minIndex]) < 0)
+
+					minIndex = j;
+
+				swap(arr, i, minIndex);
+
+			}
+		}
+
+	}
+
+	public static void swap(Object[] a, int i, int j) {
+		Object tem = a[i];
+		a[i] = a[j];
+		a[j] = tem;
+	}
+
+	public static void main(String[] args) {
+
+		Integer[] a = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+		SelectionSort.sort(a);
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println(" ");
+
+		String[] b = { "C", "D", "T", "B", "A" };
+		SelectionSort.sort(b);
+		for (int i = 0; i < b.length; i++) {
+			System.out.print(b[i] + " ");
+		}
+		System.out.println(" ");
+
+		// 测试Double
+		Double[] c = { 4.4, 3.3, 2.2, 1.1 };
+		SelectionSort.sort(c);
+		for (int i = 0; i < c.length; i++) {
+			System.out.print(c[i] + " ");
+
+		}
+		System.out.println(" ");
+
+		// 测试自定义的类 Student
+		Student[] d = new Student[4];
+		d[0] = new Student("D", 90);
+		d[1] = new Student("C", 100);
+		d[2] = new Student("B", 95);
+		d[3] = new Student("A", 95);
+		SelectionSort.sort(d);
+		for (int i = 0; i < d.length; i++) {
+			System.out.println(d[i]);
+		}
+
+	}
+
+}
