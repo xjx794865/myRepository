f4e8b325f29b3931a9d946c3ddfe5d718e22d8d7
xjx <794865413@qq.com>
2018-09-12 15:54:46 +0800

    algorithm

--- algorithm/src/algorithm/InsertionSort.java
+++ algorithm/src/algorithm/InsertionSort.java
@@ -31,13 +31,25 @@ public class InsertionSort {
 
 		}
 	}
+	
+	  // 对arr[l...r]的区间使用InsertionSort排序
+    public static void sort(Comparable[] arr, int l, int r){
+
+        for( int i = l + 1 ; i <= r ; i ++ ){
+            Comparable e = arr[i];
+            int j = i;
+            for( ; j > l && arr[j-1].compareTo(e) > 0 ; j--)
+                arr[j] = arr[j-1];
+            arr[j] = e;
+        }
+    }
 
 
 	// 测试InsertionSort
 	public static void main(String[] args) {
 
 		
-		Integer[] arr = {8,7,6,5};
+		Integer[] arr = {8,1,2,5};
 		InsertionSort.sort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
